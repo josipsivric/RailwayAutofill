c197a4673643bfb5e865319446ad79d14539e81f
Josip Sivric <josipsivric@gmail.com>
2021-07-13 13:41:06 +0200

    Fixed bug where it would fail extract data if page wasn't containing table.

--- file_operations.py
+++ file_operations.py
@@ -1,7 +1,7 @@
 """ Code for operating PDF and Excel files."""
 
 __author__ = "Josip Sivrić"
-__version__ = "1.0"
+__version__ = "1.0.1"
 __email__ = "josipsivric@gmail.com"
 __status__ = "Production"
 
@@ -19,7 +19,9 @@ def open_first_pdf(selected_pdf):
     table = []
 
     for page in pdf.pages:
-        table.extend(page.extract_table())
+        extract = page.extract_table()
+        if extract is not None:
+            table.extend(extract)
 
     new_table = table[2:]
     formated_table = [[""] * 18 for i in range(len(new_table))]
